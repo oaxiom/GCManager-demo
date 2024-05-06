02c66b1bcf614c78a72a4db438a8199157e1d464
oaxiom <oaxiom@gmail.com>
2024-05-06 14:09:15 +0800

    added stagedata

---
+++ libmanager/stagedata.py
@@ -0,0 +1,15 @@
+#
+# stagedata
+#
+# Contains information about the stages;
+
+stages = {
+        1: 'Align reads to genome',
+        2: 'BQSR correct',
+        3: 'Merge BAMs',
+        4: 'Haplotype',
+        5: 'Genotype',
+        6: 'Gather VCFs',
+        7: 'Recalibrate',
+        8: 'Finished',
+        }
