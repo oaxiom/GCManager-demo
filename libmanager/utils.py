d6a65f35688061580e00ac446a5fc0d8c0d134ad
oaxiom <oaxiom@gmail.com>
2024-06-07 08:29:47 +0800

    obfuscaate name

--- libmanager/utils.py
+++ libmanager/utils.py
@@ -14,3 +14,13 @@ def measure_disk_space(path):
     m = k / KB**1
     g = m / KB**1
     return f'{k:.2}kb', f'{m:.2}Mb', f'{g:.2}Gb'
+
+def obfuscate_name(name):
+    obf_name = []
+    for n in name.split(' '):
+        obf_name.append(f'{n[0]}*{n[-1]}')
+    return ' '.join(obf_name)
+
+if __name__ == '__main__':
+    print(obfuscate_name('王老师'))
+    print(obfuscate_name('John Smith'))
