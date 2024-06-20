c9fa47e7c23c1e00b5426933d89d352c1f5b177b
oaxiom <oaxiom@gmail.com>
2024-06-20 08:58:26 +0800

    renamed samples to PID.* format

--- demo_data/fastqs/make_minimal.sh
+++ demo_data/fastqs/make_minimal.sh
@@ -1,4 +1,4 @@
-gunzip -c full/SRR10286930_1.fastq.gz | head -n 22000000 | gzip >SRR10286930_minimal_1.fastq.gz
-gunzip -c full/SRR10286930_2.fastq.gz | head -n 22000000 | gzip >SRR10286930_minimal_2.fastq.gz
+gunzip -c full/SRR10286930_1.fastq.gz | head -n 18000000 | gzip >SRR10286930_minimal_1.fastq.gz
+gunzip -c full/SRR10286930_2.fastq.gz | head -n 18000000 | gzip >SRR10286930_minimal_2.fastq.gz
 
 
