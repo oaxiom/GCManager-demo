5edba98f90a7f8802e8c0007239a397d6a704869
oaxiom <oaxiom@gmail.com>
2024-09-10 09:51:35 +0800

    conservative pipeline setup

--- pipeline/1b.batch.sh
+++ pipeline/1b.batch.sh
@@ -1,6 +1,7 @@
 # This is for paired-end FASTQs:
 
-NPROC=$(nproc)
+NPROCall=$(nproc)
+NPROCm=$(($NPROCall-1))
 
 for p1 in *_1.fastq.gz
 do
@@ -10,6 +11,6 @@ do
 
 	echo Starting ... base \= $base \; p1 \= $p1 \; p2 \= $p2
 
-	sbatch -J $base -o $base.align.out -c $NPROC --export=ALL,base=$base,p1=$p1,p2=$p2 1b.align.slurm
+	sbatch -J $base -o $base.align.out -c $NPROCall --export=ALL,base=$base,p1=$p1,p2=$p2,NPROCm=$NPROC 1b.align.slurm
 
 done
--- pipeline/4.batch.sh
+++ pipeline/4.batch.sh
@@ -1,5 +1,8 @@
+
+NPROC=$(nproc)
+
 for chrom in chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22
 do
 	echo Submitting $chrom
-	sbatch -J haplotype.$chrom -o called.$chrom.out --export=ALL,chrom=$chrom 4.haplotype.slurm
+	sbatch -J haplotype.$chrom -o called.$chrom.out -c $NPROC --export=ALL,chrom=$chrom 4.haplotype.slurm
 done
